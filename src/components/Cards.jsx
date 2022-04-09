741cdc4aa72803a0d65f7daac2b5cb9e32a346ec
thejunglemarket <91908299+thejunglemarket@users.noreply.github.com>
2022-04-08 22:48:06 -0700

    update

---
+++ src/components/Cards.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+
+function Card(props) {
+ const [variation, setVariation] = useState(props.image1)
+ 
+    
+    function changeVariation1() {
+        setVariation(props.image1)
+    }
+    function changeVariation2() {
+        setVariation(props.image2)
+        console.log(variation)
+    }
+    function changeVariation3() {
+        setVariation(props.image3)
+        console.log(variation)
+    } 
+    function changeVariation4() {
+        setVariation(props.image4)
+        console.log(variation)
+    } 
+    function commitVariation() {
+        setVariation(props.image5)
+        console.log(variation)
+    } 
+
+return(
+
+    
+    <div className="w-full rounded overflow-hidden shadow-lg bg-slate-700 hover: hover:scale-105 hover:bg-slate-500 duration-300">
+    <img className="w-full" src={variation} alt={props.nftName}></img>
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2"><h3>NFT Name: {props.nftName}</h3></div>
+        <div className="text-slate-50 text-base">
+        <h4>Variation: {props.variationType}</h4>
+        <h5>ID: {props.id}</h5>
+        <div className="flex flex-col space-y-4 py-4">
+        <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={changeVariation1}>Variation 1</button>
+        <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={changeVariation2}>Variation 2</button>
+        <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={changeVariation3}>Variation 3</button>
+        <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={changeVariation4}>Variation 4</button>
+        <button className="align-middle rounded-lg px-4 py-2 border-4 border-spot-yellow text-spot-yellow 
+    hover:bg-spot-yellow hover:text-black duration-300 hover:border-white font-mono text-l" onClick={commitVariation}>Commit Variation</button> 
+        </div>
+        </div>
+      </div>
+      <div className="px-6 pt-4 pb-2">
+      </div>
+    </div>
+    
+    
+)
+}
+export default Card;
--- src/components/Nft.js
+++ src/components/Nft.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { useMoralis } from 'react-moralis';
 import Moralis from 'moralis';
 import Card from "./Cards";
+import img1 from "../images/1.png"
+import img2 from "../images/2.png"
+import img3 from "../images/3.png"
+import img4 from "../images/4.png"
+import img5 from "../images/3.png"
 
 
 export default function Authenticate() {
@@ -9,57 +14,63 @@ export default function Authenticate() {
     const userAddress = account
     const chain = 'avalanche';
     const spotAnalogContract = '0x0C6945E825fc3c80F0a1eA1d3E24d6854F7460d8';
-    /*const [filter, setFilter] = useState('');
-    const [nftOwned, setNftOwned] = useState([])
-    const [checkMyNfts, setCheckMyNfts] = useState([])
-    const [apiLoaded, setApiLoaded] = useState(false)
-    const [totalOwned, setTotalOwned] = useState([]);
-    const [testValue, setTestValue] = useState([]);*/
-    const [testValueStr] = useState([]);
-    const [urlImage] = useState([]);
-    const [urlString] = useState([]);
-    
 
-    
     async function getNfts() {
       const options = { address: account, chain: chain, token_address: spotAnalogContract }
       const NFTs = await Moralis.Web3API.account.getNFTsForContract(options) 
-      const setTotalOwned = NFTs.total;
-      const testValue = JSON.parse(NFTs.result[0].metadata);
-      const urlString = JSON.stringify(testValue.image);
-      console.log(urlString);
-      console.log(setTotalOwned);
-     
+        
+      console.log(JSON.stringify(NFTs.result));
+
+    }
+    
+    /*async function getNfts() {
+      const options = { address: account, chain: chain, token_address: spotAnalogContract }
+      const NFTs = await Moralis.Web3API.account.getNFTsForContract(options)
+          const result = NFTs.result
+          const final = result.map(nft=>nft.metadata)
+          let images = []
+          for(let i=0; i<final.length; i++) {
+          const token = JSON.parse(final[i])
+          images.push(token.image)
+          }
+         console.log(images)
       
-    };
-   
-
+    };*/
     useEffect(() => {
-       getNfts();
-    }, [account])
-   
-
+      getNfts();
+    },[]);
+    
     return (
       <div className="p-10 grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 font-mono text-spot-yellow bg-slate-900">
         <Card
           key = "This1"
           nftName = "That"
           variationType = "What"
-          image = {urlString}
+          image1 = {img1}
+          image2 = {img2}
+          image3 = {img3}
+          image4 = {img4}
+          image5 = {img2}
           id = "this"
           />
         <Card
           key = "This2"
           nftName = "That"
           variationType = "What"
-          image = "https://assets.nftrade.com/image/upload/w_500,c_scale/v1645587526/evm_43114_0x19a5b9fcd30d067c8fc287fe81b08798b837a3c6_1.png"
+          image1 = {img1}
+          image2 = {img2}
+          image3 = {img3}
+          image4 = {img4}
+          image5 = {img2}
           id = "this"
           />
         Nfts in your wallet: 
         
         </div>
-         
+          
   
       )
+
+    
          
     }
