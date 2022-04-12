6e8052250215cd0c1b4365161cdfa1b2c0ec36bc
thejunglemarket <91908299+thejunglemarket@users.noreply.github.com>
2022-04-11 21:15:20 -0700

    minting function

--- src/components/Nft.js
+++ src/components/Nft.js
@@ -1,18 +1,15 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react';
-import { useMoralis } from 'react-moralis';
-import Moralis from 'moralis';
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import { useMoralis } from "react-moralis";
+import Moralis from "moralis";
 import Card from "./Cards";
-import img1 from "../images/1.png"
-import img2 from "../images/2.png"
-import img3 from "../images/3.png"
-import img4 from "../images/4.png"
-import { data } from 'autoprefixer';
-
-
-
+import img1 from "../images/1.png";
+import img2 from "../images/2.png";
+import img3 from "../images/3.png";
+import img4 from "../images/4.png";
+import { data } from "autoprefixer";
 
 export default function Authenticate() {
-    /*console.log("component rendering")
+  /*console.log("component rendering")
     const {account, isAuthenticated} = useMoralis();
     const userAddress = account
     const chain = 'avalanche';
@@ -49,28 +46,30 @@ export default function Authenticate() {
     
 */
 
-function createCard(collectionCard) { //Building the card here from Card.jsx passing props and simultaneously fetching traits on click.
-  return (
-      
-     <div onClick= {()=>{
-
-     }}> <Card
-      key = {collectionCard.id}
-      nftName = {collectionCard.nftName}
-      traitType = {collectionCard.traitType}
-      rarity = {collectionCard.rarity}
-      image = {collectionCard.image}
-      id = {collectionCard.id}
-      /></div>
-    )
-    }
-
+  function createCard(collectionCard) {
+    //Building the card here from Card.jsx passing props and simultaneously fetching traits on click.
     return (
-      <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4 xl:grid-cols-6 gap-5 font-mono text-spot-yellow">
-      {createCard}
+      <div onClick={() => {}}>
+        {" "}
+        <Card
+          key={collectionCard.id}
+          nftName={collectionCard.nftName}
+          traitType={collectionCard.traitType}
+          rarity={collectionCard.rarity}
+          image={collectionCard.image}
+          id={collectionCard.id}
+        />
       </div>
-    )}
-     /* <div className="p-10 grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 font-mono text-spot-yellow bg-slate-900">
+    );
+  }
+
+  return (
+    <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4 xl:grid-cols-6 gap-5 font-mono text-spot-yellow">
+      {createCard}
+    </div>
+  );
+}
+/* <div className="p-10 grid grid-cols-2 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 font-mono text-spot-yellow bg-slate-900">
         <Card
           key = "1"
           nftName = "insert nft name prop"
@@ -98,6 +97,3 @@ function createCard(collectionCard) { //Building the card here from Card.jsx pas
   
       )
 */
-    
-         
-
